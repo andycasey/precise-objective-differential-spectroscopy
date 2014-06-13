5ec3e411a7cf4a35eae0b94b5188c9b2248c25f6
Andy Casey <andycasey@gmail.com>
2014-06-13 20:49:17 +0100

    Initialise Travis

---
+++ setup.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+""" This project needs a name. """ 
+
+import os
+import re
+import sys
+
+try:
+    from setuptools import setup
+
+except ImportError:
+    from distutils.core import setup
+
+major, minor1, minor2, release, serial =  sys.version_info
+open_kwargs = {"encoding": "utf-8"} if major >= 3 else {}
+
+def readfile(filename):
+    with open(filename, **open_kwargs) as fp:
+        contents = fp.read()
+    return contents
+
+version_regex = re.compile("__version__ = \"(.*?)\"")
+contents = readfile(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "unnamed", "__init__.py"))
+
+version = version_regex.findall(contents)[0]
+
+setup(name="unnamed",
+    version=version,
+    author="Andrew R. Casey",
+    author_email="arc@ast.cam.ac.uk",
+    packages=["unnamed"],
+    url="http://www.github.com/andycasey/honest-spectroscopy/",
+    license="MIT",
+    description="Stellar spectroscopists are (largely) doing it wrong.",
+    long_description=readfile(os.path.join(os.path.dirname(__file__), "README.md")),
+    install_requires=readfile(
+        os.path.join(os.path.dirname(__file__), "requirements.txt")).split("\n"),
+)
---
+++ unnamed/__init__.py
@@ -0,0 +1,7 @@
+# coding: utf-8
+
+""" Honest spectroscopy """
+
+__author__ = "Andy Casey <arc@ast.cam.ac.uk>"
+__version__ = "0.01alpha"
+
