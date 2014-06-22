b98f490849f72e982a7f4d58ee3c74202cd5206a
Andy Casey <andycasey@gmail.com>
2014-06-22 19:23:27 -0400

    Basic optimisation working

--- code/stellar_parameters.py
+++ code/stellar_parameters.py
@@ -14,6 +14,7 @@ import numpy as np
 import numpy.lib.recfunctions as nprcf
 from scipy import optimize as op, ndimage
 
+import atmosphere
 import model
 import moog
 import specutils
@@ -24,12 +25,31 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
+
+def sp_jacobian(stellar_parameters, *args):
+    """ Approximate the Jacobian of the stellar parameters and
+    minimisation parameters, based on calculations from the Sun """
+
+    logger.info("Updated approximation of the Jacobian")
+
+    teff, xi, logg, m_h = stellar_parameters
+
+    # This is the black magic.
+    jacobian = np.array([
+        [ 5.4393e-08*teff - 4.8623e-04, -7.2560e-02*xi + 1.2853e-01,  1.6258e-02*logg - 8.2654e-02,  1.0897e-02*m_h - 2.3837e-02],
+        [ 4.2613e-08*teff - 4.2039e-04, -4.3985e-01*xi + 8.0592e-02, -5.7948e-02*logg - 1.2402e-01, -1.1533e-01*m_h - 9.2341e-02],
+        [-3.2710e-08*teff + 2.8178e-04,  3.8185e-03*xi - 1.6601e-02, -1.2006e-02*logg - 3.5816e-03, -2.8592e-05*m_h + 1.4257e-03],
+        [-1.7822e-08*teff + 1.8250e-04,  3.5564e-02*xi - 1.1024e-01, -1.2114e-02*logg + 4.1779e-02, -1.8847e-02*m_h - 1.0949e-01]
+    ])
+    return jacobian.T
+
+
 class Star(object):
     """
     A model star.
     """
 
-    def __init__(self, atmospheres, transitions, spectra, **kwargs):
+    def __init__(self, model_atmospheres, transitions, spectra, **kwargs):
         """
         Create a model star.
 
@@ -42,7 +62,6 @@ class Star(object):
         self.spectra = [spectra[index] for index in np.argsort([spectrum.dispersion[0] \
             for spectrum in spectra])]
 
-        self.atmospheres = atmospheres
         self.transitions = transitions
 
         # Create spectral channels for each spectrum, and associate transitions to each.
@@ -57,12 +76,21 @@ class Star(object):
             self.channels.append(SpectralChannel(spectrum, transitions[indices], **kwargs))
             previous_order_max_disp = max_disp
 
+        # Create the atmosphere interpolator.
+        if model_atmospheres not in atmosphere.parsers:
+            raise KeyError("No matching model atmospheres found. Installed types are: {0}".format(
+                ", ".join(atmosphere.parsers.keys())))
+        folder, parser = atmosphere.parsers[model_atmospheres]
+        self.model_atmospheres = atmosphere.AtmosphereInterpolator(folder, parser())
+
         # Create a list of parameters.
-        self.parameters = ["teff", "logg", "[M/H]", "xi"]
+
+
+        self.parameters = ["teff", "logg", "[M/H]", "[alpha/M]", "xi"]
         return None
 
 
-    def optimise_sp(self, p0=None, elements=("Fe", ), line_outliers=True, **kwargs):
+    def optimise_sp(self, p0=None, elements=("Fe", ), line_outliers=True, moog_kwargs=None, **kwargs):
         """
         Calculate equivalent widths for all transitions in all channels, then optimise the stellar
         parameters.
@@ -71,6 +99,9 @@ class Star(object):
         if p0 is None:
             raise NotImplementedError("need to test limits with an atmosphere interpolator")
 
+        if moog_kwargs is None:
+            moog_kwargs = {}
+
         # Get the list of neutral and ionized species for stellar parameter optimisation.
         species = map(utils.element_to_species, elements)
         species += [0.1 + each for each in species]
@@ -78,7 +109,12 @@ class Star(object):
         # Measure equivalent widths in all channels.
         balance_transitions = []
         for i, channel in enumerate(self.channels):
-            xopt, fopt, model_flux = channel.optimise(**kwargs)
+            if "plot_filename" in kwargs:
+                channel_kwargs = kwargs.copy()
+                channel_kwargs["plot_filename"] = channel_kwargs["plot_filename"].format(i)
+            else:
+                channel_kwargs = kwargs.copy()
+            xopt, fopt, model_flux = channel.optimise(**channel_kwargs)
 
             # Identify all the species we need.
             species_indices = np.zeros(len(channel.transitions), dtype=bool)
@@ -86,7 +122,7 @@ class Star(object):
                 species_indices += (channel.transitions["species"] == each)
             balance_transitions.append(channel.transitions[species_indices])
 
-        balance_transitions = nprcf.merge_arrays(*balance_transitions)
+        balance_transitions = nprcf.stack_arrays(balance_transitions, usemask=False)
 
         # Use the table of measured equivalent widths to optimise stellar parameters.
         # We'll need an instance of MOOG for this.
@@ -96,18 +132,37 @@ class Star(object):
             # Write transitions and equivalent widths to file.
             ew_filename = os.path.join(moogsilent.twd, "ews")
             with open(ew_filename, "w+") as fp:
-                fp.write(moogsilent._format_ew_input(balance_transitions, **kwargs))
+                fp.write(moogsilent._format_ew_input(balance_transitions, **moog_kwargs))
             
-            abundances = moogsilent.abfind("marcs-sun.model", ew_filename, **kwargs)
+            """
+            abundances = moogsilent.abfind("marcs-sun.model", ew_filename, **moog_kwargs)
             plt.scatter(np.log(abundances["equivalent_width"]/abundances["wavelength"]), abundances["abundance"])
             raise a
-            
+            """
+
+            fill_value = 1.
+
             def minimise(theta):
+
+                # Parse theta
+                teff, xi, logg, m_h= theta[:4]
+                alpha_m = 0.
+
+                if 0 > xi: return np.array([fill_value] * 4)
 
                 # Check to see if we have already executed MOOG for these parameters given some
                 # tolerance, so that we can avoid unnecessary calls to MOOG.
 
                 # Interpolate a stellar atmosphere and write it to file.
+                atmosphere_filename = os.path.join(moogsilent.twd, "model")
+                try:
+                    thermal_structure = self.model_atmospheres.interpolate_thermal_structure(
+                        teff, logg, m_h, alpha_m)
+                except:
+                    return np.array([fill_value] * 4)
+
+                self.model_atmospheres.parser.write_atmosphere(atmosphere_filename,
+                    teff, logg, m_h, xi, thermal_structure, clobber=True)
 
                 # Convert equivalent widths to abundances.
                 results = moogsilent.abfind(atmosphere_filename, ew_filename, **kwargs)
@@ -119,33 +174,46 @@ class Star(object):
                 # - input model atmosphere - mean Fe abundance?
                 # - mean Fe abundance - mean Fe II abundance?
                 excitation_balance = line.Line(x=results["excitation_potential"], y=results["abundance"],
-                    outliers=line_outliers, yerr=[0.05] * len(results), ).optimise()
+                    yerr=np.array([0.05] * len(results)), outliers=line_outliers).optimise()
 
                 turbulence_balance = line.Line(x=np.log(results["equivalent_width"]/results["wavelength"]),
-                    y=results["abundance"], yerr=[0.05] * len(results), outliers=line_outliers).optimise()
+                    y=results["abundance"], yerr=np.array([0.05] * len(results)), outliers=line_outliers)\
+                    .optimise()
 
-                # Find neutral and ionised speces
-                int_species = map(int, results["species"])
+                # Find neutral and ionised species.
+                int_species = np.array(map(int, results["species"]))
                 ionised = results["species"] % int_species > 0
-                neutral = not ionised
+                neutral = ~ionised
                 # Match by species. 
-                ionisation_balance = 0           
+                ionisation_balance = 0
                 for specie in set(map(int, species)):
                     neutral_species = (int_species == specie) * neutral
                     ionised_species = (int_species == specie) * ionised
-                    ionisation_balance += (np.mean(neutral_species["abundance"]) \
-                        - np.mean(ionised_species["abundance"]))**2
+                    ionisation_balance += (np.median(results[neutral_species]["abundance"]) \
+                        - np.median(results[ionised_species]["abundance"]))/10.
 
+                metallicity_balance = np.median((results[(results["species"] == species[0])]["abundance"] \
+                    - (m_h + 7.50))/10.) # TODO
 
                 # Return a sum of slopes.
+                components = np.array([excitation_balance[0], turbulence_balance[0], ionisation_balance,
+                    metallicity_balance])**2
 
-                None
+                print(theta, np.sum(components))
+                return components
 
             # Run minimise for p0.
+            def scalar_min(x):
+                result = np.sum(minimise(x))
+                print(x, result)
+                return result
 
             # scipy optimise
+            result = op.fsolve(minimise, [5777, 1.06, 4.40, 0.01], fprime=sp_jacobian, col_deriv=1, epsfcn=0,
+                xtol=1e-10, full_output=1, maxfev=100)
 
-        return None
+            raise a
+        return result
 
 
     def infer(self, ):
@@ -302,7 +370,8 @@ class SpectralChannel(object):
         return chi_sq
 
 
-    def optimise(self, force=False, verbose=False, line_kwargs=None, channel_kwargs=None):
+    def optimise(self, force=False, verbose=False, line_kwargs=None, channel_kwargs=None, plot_filename=None,
+        plot_clobber=False):
         # Optimise the channel and line parameters.
 
         op_line_kwargs = { "xtol": 1e-8, "maxfun": 10e3, "maxiter": 10e3,
@@ -407,23 +476,6 @@ class SpectralChannel(object):
 
         default_p0["smoothing_sigma"] = abs(np.median([xopt[1] for xopt in xopts]))
         
-        """
-        # Produce a plot.
-        continuum = continuum_shape.copy()
-        for transition, result in zip(self.transitions, line_fits):
-            if self.wl_tolerance > 0:
-                depth, sigma, wavelength = result["x"]
-            else:
-                depth, sigma = result["x"]
-                wavelength = transition["rest_wavelength"]
-
-            continuum *= self._absorption_line_(wavelength, depth, sigma)
-
-        plt.plot(self.dispersion, self.data, 'k')
-        plt.plot(self.dispersion, continuum, 'b')
-        raise a
-        """
-
         # Optimise the parameters globally if required.
         # Note: the smoothing kernel is always a global parameter, but the median of the line sigmas
         #       is a sufficient approximation. So here we are just checking for either redshift or
@@ -457,6 +509,28 @@ class SpectralChannel(object):
 
         fopt, model = self._fit(xopt, True)
 
+        if plot_filename is not None:
+
+            # Produce a plot.
+            """
+            continuum = continuum_shape.copy()
+            for transition, result in zip(self.transitions, line_fits):
+                if self.wl_tolerance > 0:
+                    depth, sigma, wavelength = result["x"]
+                else:
+                    depth, sigma = result["x"]
+                    wavelength = transition["rest_wavelength"]
+
+                continuum *= self._absorption_line_(wavelength, depth, sigma)
+            """
+            fig, ax = plt.subplots(figsize=(50, 2))
+            ax.plot(self.dispersion, self.data, 'k')
+            ax.plot(self.dispersion, model, 'b')
+            ax.set_xlim(self.dispersion[0], self.dispersion[-1])
+            ax.set_ylim(0.5, 1.2)
+            fig.savefig(plot_filename, clobber=plot_clobber)
+            plt.close(fig)
+
         # Calculate equivalent widths in the same units as the dispersion.
         logging.warn("Assuming spectral dispersion units are Angstroms. Measured equivalent widths stored as mA.")
         self.transitions["equivalent_width"] = np.array([xopt[self.parameters.index("ld_{0}".format(i))] \
@@ -472,19 +546,42 @@ class SpectralChannel(object):
 
 
 
-
-
-
-data = np.loadtxt("spectra/uvessun2.txt", skiprows=1)
-
 class spectrum(object):
     pass
+
+
+data = np.loadtxt("spectra/uvessun1.txt", skiprows=1)
 
 blue_channel = spectrum()
 blue_channel.dispersion = data[:,0]
 blue_channel.flux = data[:, 1]
 blue_channel.variance =  np.array([0.001] * len(blue_channel.dispersion))
 blue_channel.inv_var = 1.0/blue_channel.variance
+
+
+data = np.loadtxt("spectra/uvessun2.txt", skiprows=1)
+
+green_channel = spectrum()
+green_channel.dispersion = data[:,0]
+green_channel.flux = data[:, 1]
+green_channel.variance =  np.array([0.001] * len(green_channel.dispersion))
+green_channel.inv_var = 1.0/green_channel.variance
+
+data = np.loadtxt("spectra/uvessun3.txt", skiprows=1)
+
+red_channel = spectrum()
+red_channel.dispersion = data[:,0]
+red_channel.flux = data[:, 1]
+red_channel.variance =  np.array([0.001] * len(red_channel.dispersion))
+red_channel.inv_var = 1.0/red_channel.variance
+
+data = np.loadtxt("spectra/uvessun4.txt", skiprows=1)
+
+ir_channel = spectrum()
+ir_channel.dispersion = data[:,0]
+ir_channel.flux = data[:, 1]
+ir_channel.variance =  np.array([0.001] * len(ir_channel.dispersion))
+ir_channel.inv_var = 1.0/ir_channel.variance
 
 #sun = Star([blue_channel], "marcs-sun.model")#, line_lists)
 #sun.optimise_channel(blue_channel, "marcs-sun.model", transitions)
@@ -498,7 +595,7 @@ indices = (5875. > transitions["rest_wavelength"]) * (transitions["rest_waveleng
 blue = SpectralChannel(blue_channel, transitions[indices], wl_tolerance=0.1)
 #result = blue.optimise(verbose=True)
 
-star = Star(None, transitions, [blue_channel], wl_tolerance=0.1)
+star = Star("MARCS (2011)", transitions, [blue_channel, green_channel, red_channel, ir_channel], wl_tolerance=0.1)
 
 # Now we have equivalent widths. Use them to optimise stellar parameters.
 
